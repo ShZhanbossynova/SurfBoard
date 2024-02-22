5ca65b26579be15e8742a5da9e65a92ca3c86854
Shinar Zhanbosynova <150686687+ShZhanbossynova@users.noreply.github.com>
2024-02-22 10:03:05 +0300

    Week 4.1

--- script.js
+++ script.js
@@ -35,4 +35,74 @@ function toggleMenu(element) {
   body.classList.toggle('body--active-menu');
 }
 
-burger.addEventListener('click',  toggleMenu);
+burger.addEventListener('click', toggleMenu);
+
+//Accordeon
+function accordeonTeam() {
+  const workers = document.querySelectorAll(".accordeon__item");
+  const teamAccord = document.querySelector(".accordeon");
+
+  teamAccord.addEventListener("click", function (event) {
+    event.preventDefault(); // скидываем стандартное состояние (что бы не кидало страницу вверх / или не перенаправляло на другую)
+    const target = event.target; // то на что мы клацнули
+   
+    if (target.classList.contains("accordeon__link")) {
+      const worker = target.parentNode; // родитель нашей ссылки (li.accordeon__item)
+      const content = target.nextElementSibling; //  элемент который находится рядом с нашей ссылкой на уровне (accordeon__content)
+      const contentHeight = content.firstElementChild.clientHeight; // высота wrapper который лежит в accordeon__content
+
+      for (const iterator of workers) {
+        if (iterator !== worker) {
+          iterator.classList.remove("accordeon__item-active");
+          iterator.lastElementChild.style.height = 0;
+        }
+      }
+
+      if (worker.classList.contains("accordeon__item-active")) {
+        worker.classList.remove("accordeon__item-active");
+        content.style.height = 0;
+      } else {
+        worker.classList.add("accordeon__item-active");
+        content.style.height = contentHeight + "px";
+      }
+    }
+  });
+}
+
+accordeonTeam();
+
+//Reviews
+
+const reviewsBar = document.querySelector('.reviews__switcher');
+const reviewsItems = document.querySelectorAll('.reviews__switcher-item');
+const reviewWrapp = document.querySelectorAll('.reviews__display');
+
+reviewsBar.addEventListener('click' , e=>{
+   e.preventDefault();
+  
+  // console.log(Array.from(reviewsBar.children))
+  
+  if(e.target.classList.contains('reviews__link')){
+    const currentPeopleLink = e.target;
+    const currentPeopleItem = currentPeopleLink.closest('.reviews__item')
+    const currentPeopleId = currentPeopleItem.id;
+    
+    for (const iterator of reviewsItems) {
+      if(iterator !== currentPeopleItem){
+        iterator.classList.remove("reviews__item--active");
+      }
+    }
+    
+    if(!currentPeopleItem.classList.contains('reviews__item--active')){
+      currentPeopleItem.classList.add("reviews__item--active");
+    }
+    
+    reviewWrapp.forEach(function(wrapp){
+      if(currentPeopleId == wrapp.id){
+        wrapp.classList.add('reviews__wrap--active');
+      }else{
+        wrapp.classList.remove('reviews__wrap--active');
+      }
+    })
+  }
+})
